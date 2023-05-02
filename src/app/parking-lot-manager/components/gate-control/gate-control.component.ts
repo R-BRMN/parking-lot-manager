fdec5e9a4df54c8a82f01fdba989f3219160bfe0
RBRMN <ronnie.dov@gmail.com>
2023-05-03 00:32:38 +0300

    add gate control component template

---
+++ src/app/parking-lot-manager/components/gate-control/gate-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gate-control',
+  templateUrl: './gate-control.component.html',
+  styleUrls: ['./gate-control.component.css']
+})
+export class GateControlComponent {
+
+}
